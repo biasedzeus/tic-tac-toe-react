e8b7dacd865ebc59fa63babca27c9fc72d358132
Bhanu Singh <bhanu.singh1078@gmail.com>
2021-11-22 11:14:59 +0530

    npx app created

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import './style.css';
+import axios from 'axios';
+
+export default function App() {
+  function handleLogin() {
+    axios.get();
+  }
+
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+
+    </div>
+  );
+}
