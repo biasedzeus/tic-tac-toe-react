170ce0d428f0610dd75146d2a53a4b3745d19cc6
Bhanu Singh <bhanu.singh1078@gmail.com>
2021-11-22 11:38:52 +0530

    class comp changed to func comp

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,24 +1,11 @@
-import React from 'react'
+import React from "react";
 
+function Square(props) {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
+}
 
-export default class Square extends React.Component {
-//   constructor(props){
-//       super(props);
-//       this.state = {
-//           value:null,
-//       }
-//   }
-    
-    
-    
-    render() {
-      return (
-        <button 
-        className="square"
-        onClick={() =>this.props.onClick()}
-        >
-          {this.props.value}
-        </button>
-      );
-    }
-  }
+export default Square;
