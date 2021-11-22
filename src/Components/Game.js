187097bfc8fe8e78a22889d53d99733d3aaa4936
Bhanu Singh <bhanu.singh1078@gmail.com>
2021-11-22 12:46:24 +0530

    movehistory added

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -31,7 +31,21 @@ export default class Game extends React.Component {
       const winner = calculateWinner(current.squares);
 
 
-    
+    const moves = history.map((step,move) =>{
+        const desc = move ?
+        'Go to Move #' + move :
+        'Go to game start';
+
+        return(
+            <li>
+                <button
+                 onClick={() => this.jumpTo(move)}
+                >
+                    {desc}
+                </button>
+            </li>
+        )
+    })
 
 
       let status;
@@ -56,3 +70,22 @@ export default class Game extends React.Component {
     );
   }
 }
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
