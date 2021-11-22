1ad92d0f13abea2565ba3c71deef37c78364be69
Bhanu Singh <bhanu.singh1078@gmail.com>
2021-11-22 11:26:30 +0530

    state added ,intially set to null

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -2,10 +2,22 @@ import React from 'react'
 
 
 export default class Square extends React.Component {
+  constructor(props){
+      super(props);
+      this.state = {
+          value:null,
+      }
+  }
+    
+    
+    
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button 
+        className="square"
+        onClick={() => console.log("Clicked")}
+        >
+          {this.props.value}
         </button>
       );
     }
