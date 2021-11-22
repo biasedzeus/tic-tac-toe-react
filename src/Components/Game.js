55cd94cdaa1ce1630fc791dd016ed334b8b42669
Bhanu Singh <bhanu.singh1078@gmail.com>
2021-11-22 12:52:50 +0530

    time travel added

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -7,10 +7,12 @@ export default class Game extends React.Component {
     this.state = {
       history: [{ squares: Array(9).fill(null) }],
       xIsNext: true,
+      stepNumber: 0,
+
     };
   }
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0,this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -22,12 +24,20 @@ export default class Game extends React.Component {
         squares: squares
       }]),
       xIsNext: !this.state.xIsNext,
+      stepNumber:history.length,
     });
+  }
+
+  jumpTo(step){
+      this.setState({
+          stepNumber:step,
+          xIsNext:(step % 2) === 0,
+      })
   }
 
   render() {
       const history = this.state.history;
-      const current= history[history.length - 1];
+      const current= history[this.state.stepNumber];
       const winner = calculateWinner(current.squares);
 
 
@@ -37,7 +47,7 @@ export default class Game extends React.Component {
         'Go to game start';
 
         return(
-            <li>
+            <li key={move}>
                 <button
                  onClick={() => this.jumpTo(move)}
                 >
@@ -64,7 +74,7 @@ export default class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
